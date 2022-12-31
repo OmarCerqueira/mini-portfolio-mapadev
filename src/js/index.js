6753542766a15f10849db6d488e5d7b244f6c10c
OmarCerqueira <104476765+OmarCerqueira@users.noreply.github.com>
2022-12-31 00:27:30 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,57 @@
+/*
+    OBJETIVO - Quando clicarmos na aba temos que mostrar o conteúdo da aba que foi clicada pelo usuario e esconder o conteudo da aba anterior
+
+    Passo 1 - Dar um jeito de pegar os elementos que representam as abas no HTML
+
+    Passo 2 - Dar um jeito de identificar o clique no elemento da aba
+
+    Passo 3 - Quando o usuario clicar, desmarcar a aba selecionada
+
+    Passo 4 - Marcar a aba clicada como selecionada
+
+    Passo 5 - Esconder o conteudo anterior
+       
+    Passo 6 - Mostrar o conteudo da aba selecionada
+
+*/
+
+
+// Passo 1 - Dar um jeito de pegar os elementos que representam as abas no HTML
+
+const abas = (document.querySelectorAll(".aba"));
+
+
+abas.forEach(aba => {
+    // Passo 2 - Dar um jeito de identificar o clique no elemento da aba
+    aba.addEventListener('click', function(){
+        
+        if(aba.classList.contains('selecionado')){
+            return;
+        }
+        
+        selecionarAba(aba)
+        mostrarInformacoesDaAba(aba)
+    })
+})
+
+function selecionarAba(aba){
+    // Passo 3 - Quando o usuario clicar, desmarcar a aba selecionada
+    const abaSelecionada = (document.querySelector('.aba.selecionado'))
+    abaSelecionada.classList.remove('selecionado')
+
+    // Passo 4 - Marcar a aba clicada como selecionada
+    aba.classList.add('selecionado')
+}
+
+function mostrarInformacoesDaAba(aba){
+    // Passo 5 - Esconder o conteudo anterior
+    const informacaoSelecionada = (document.querySelector('.informacao.selecionado'))
+    console.log(informacaoSelecionada);
+    informacaoSelecionada.classList.remove('selecionado')
+
+    // Passo 6 - Mostrar o conteudo da aba selecionada
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+    
+    const informacaoASerMostrada = (document.getElementById(idDoElementoDeInformacoesDaAba))
+    informacaoASerMostrada.classList.add('selecionado')
+}
